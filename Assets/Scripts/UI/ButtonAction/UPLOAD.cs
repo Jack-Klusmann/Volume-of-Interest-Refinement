d17db90923b82aec6c3d52fa3990be34f545c1bb
Jack Sterling Klusmann <Jack.Klusmann@icloud.com>
2023-03-27 19:24:18 +0200

    Revert "Unity stuff"
    This reverts commit 8ac3866b432afeba2110ec17505505de38b6c564.

--- Assets/Scripts/UI/ButtonAction/UPLOAD.cs
+++ Assets/Scripts/UI/ButtonAction/UPLOAD.cs
@@ -6,21 +6,13 @@ public class UPLOAD : MonoBehaviour
 {
     [SerializeField] private VoxelGridVisualizer voxelGridVisualizer;
     [SerializeField] private PopupMessage popupMessage;
-    [SerializeField] private Settings settings;
 
     public void OnClick()
     {
         if (voxelGridVisualizer.meshExists)
         {
-            if (settings.getBoolByName("Local Mode"))
-            {
-                
-            }
-            else
-            {
-                voxelGridVisualizer.uploadToserverAndVisualize();
-                popupMessage.PopUp("UPLOADING...");
-            }
+            voxelGridVisualizer.uploadToserverAndVisualize();
+            popupMessage.PopUp("UPLOADING...");
         }
         else
         {
